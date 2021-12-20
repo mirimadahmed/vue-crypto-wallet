a3b5fddbbe15443b6a1632a17bbd70c193832733
Mir <mirimadahmed@outlook.com>
2021-12-21 00:31:19 +0500

    pwa did

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './registerServiceWorker'
 
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 const bootstrapSassAbstractsImports = require('vue-cli-plugin-bootstrap-vue/sassAbstractsImports.js')
 module.exports = {
-	css: {
-		loaderOptions: {
-			sass: {
-				additionalData: bootstrapSassAbstractsImports.join('\n')
-			},
-			scss: {
-				additionalData: [...bootstrapSassAbstractsImports, ''].join(';\n')
-			}
-		}
-	}
+    css: {
+        loaderOptions: {
+            sass: {
+                additionalData: bootstrapSassAbstractsImports.join('\n')
+            },
+            scss: {
+                additionalData: [...bootstrapSassAbstractsImports, ''].join(';\n')
+            }
+        }
+    },
 }

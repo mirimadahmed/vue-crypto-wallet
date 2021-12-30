b76b37e83861068a3faf7b7c417d00ce83b16488
Mir <mirimadahmed@outlook.com>
2021-12-30 19:05:25 +0500

    2fa fix

--- src/views/2FA.vue
+++ src/views/2FA.vue
@@ -2,25 +2,46 @@
   <div class="col-md-6 m-auto">
     <div class="row p-4 m-0 mt-5 login-wrapper bg-light">
       <div class="col-12">
-        <h2 class="p-2 mt-3 text-center">{{ $t('auth.2fa.heading') }}</h2>
+        <h2 class="p-2 mt-3 text-center">{{ $t("auth.2fa.heading") }}</h2>
       </div>
       <div class="col-12">
         <b-form-group
           id="input-group-2"
-          :label="`${ $t('auth.2fa.qr') }`"
+          :label="`${$t('auth.2fa.otp_placeholder')}`"
           label-for="input-2"
           class="text-center"
         >
           <img :src="qr" alt="Scan qr here" />
         </b-form-group>
       </div>
+      <div class="col-12 d-md-block d-lg-none">
+        <b-form-group
+          id="input-group-2"
+          :label="`${$t('auth.2fa.copy')}`"
+          label-for="input-2"
+          class="text-center p-2 mt-3"
+        >
+        <span style="overflow-wrap: anywhere;" class="text-muted">
+          {{ text.substr(0,7) }} ...
+          <copy-to-clipboard :text="text" @copy="handleCopy">
+            <a href="#"><b-icon icon="clipboard-check"></b-icon></a>
+          </copy-to-clipboard>
+        </span>
+        </b-form-group>
+      </div>
       <div class="col-12">
-        <b-form-group id="input-group-1" :label="`${ $t('auth.2fa.otp_label') }`" label-for="input-1">
+        <b-form-group
+          id="input-group-1"
+          :label="`${$t('auth.2fa.otp_label')}`"
+          label-for="input-1"
+        >
           <b-form-input
+            autofocus
+            @keydown.enter="verify()"
             id="input-1"
             v-model="state"
             type="text"
-            :placeholder="`${ $t('auth.2fa.otp_placeholder') }`"
+            :placeholder="`${$t('auth.2fa.otp_placeholder')}`"
             required
           ></b-form-input>
         </b-form-group>
@@ -42,7 +63,7 @@
             v-if="isLoading"
           ></span>
           <span class="sr-only" v-if="isLoading">Loading...</span>
-          {{ $t('auth.2fa.button') }}
+          {{ $t("auth.2fa.button") }}
         </b-button>
       </div>
     </div>
@@ -50,15 +71,20 @@
 </template>
 
 <script>
+import CopyToClipboard from "vue-copy-to-clipboard";
 import MoralisFactory from "@/moralis";
 const moralis = MoralisFactory();
 import speakeasy from "speakeasy";
 import QRCode from "qrcode";
 export default {
+  components: {
+    CopyToClipboard,
+  },
   data() {
     return {
       user: null,
       secret: null,
+      text: null,
       qr: null,
       state: null,
       verified: false,
@@ -95,15 +121,18 @@ export default {
     generateTempSecret() {
       let secret = speakeasy.generateSecret();
       this.secret = secret.ascii;
+      console.log(this.secret);
       this.user.set("tempSecret", this.secret);
       this.otpauth_url = speakeasy.otpauthURL({
         secret: this.secret,
         label: "Sardis Wallet",
         algorithm: "sha512",
       });
-
+      this.text = this.otpauth_url.split("=")[1].split("&")[0];
+      console.log(this.otpauth_url);
       QRCode.toDataURL(this.otpauth_url, (err, data_url) => {
         this.qr = data_url;
+        console.log(this.qr);
       });
     },
     verify() {
@@ -130,6 +159,13 @@ export default {
       } else {
         this.error = "Unable to verify that OTP.";
       }
+    },
+    handleCopy() {
+      this.$bvToast.toast("Copied to clipboard", {
+        title: "Copy",
+        variant: "info",
+        solid: true,
+      });
     },
   },
 };
